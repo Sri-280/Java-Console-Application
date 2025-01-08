19f9a82d23cb6e5224eda5a4946ca9bb58171e37
Sri-280 <srinika280@gmail.com>
2025-01-08 16:01:22 +0530

    08.01.2025

---
+++ Listofnotes/Note200.java
@@ -0,0 +1,9 @@
+package Listofnotes;
+
+import Notes.Notes;
+
+public class Note200 extends Notes {
+    public Note200(String note,int count){
+        super(note,count);
+    }
+}
---
+++ Listofnotes/Note500.java
@@ -0,0 +1,9 @@
+package Listofnotes;
+
+import Notes.Notes;
+
+public class Note500 extends Notes {
+    public Note500(String note,long count){
+        super(note,count);
+    }
+}
