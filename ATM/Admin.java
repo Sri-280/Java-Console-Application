f4972b52734c884778a3d15d3d73e9ddf20735de
Sri-280 <srinika280@gmail.com>
2025-01-13 10:08:23 +0530

    13.01.2025

---
+++ ATM/Admin.java
@@ -0,0 +1,27 @@
+package ATM;
+
+import java.util.ArrayList;
+
+public class Admin extends Accounts{
+//    private final String adminId;
+//    private final String adminPin;
+//    private final ArrayList<Transactions> adminTransactions = new ArrayList<>(); //to store
+
+    public Admin(String adminId, String adminPin) {
+        super(adminId,adminPin);
+//        this.adminId = adminId;
+//        this.adminPin = adminPin;
+//    }
+//    public String getAdmin_Id() {
+//        return adminId;
+//    }
+//    public String getAdmin_Pin() {
+//        return adminPin;
+//    }
+//    public ArrayList<Transactions> getAdminTransactions() {
+//        return adminTransactions;
+    }
+//    public static boolean checkAdmin(String adminId, String adminPin) {
+//        return adminId.equals("admin") && adminPin.equals("1234");
+//    }
+}
